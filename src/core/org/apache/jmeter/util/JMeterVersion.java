8efcc9ffb6ca2dc023b8144ac626e97062e3010e
Jonathon Roberts <jroberts@bodymedia.com>
2011-05-04 11:15:03 -0400

    Added ExpandAll/collapseAll buttons
    Conflicts:
    
    	src/core/org/apache/jmeter/util/JMeterVersion.java

--- src/core/org/apache/jmeter/util/JMeterVersion.java
+++ src/core/org/apache/jmeter/util/JMeterVersion.java
@@ -44,7 +44,9 @@ public class JMeterVersion {
      * This ensures that JMeterUtils always gets the correct
      * version, even if JMeterUtils is not re-compiled during the build.
      */
-    private static final String VERSION = "2.3.4";
+
+    private static final String VERSION = "2.4.20110504";
+
 
     static final String COPYRIGHT = "Copyright (c) 1998-2009 The Apache Software Foundation";
 
